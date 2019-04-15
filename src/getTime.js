dc8a615ce4eb274caeb1cfb57c34bff88317f771
Alexei Belous <belous.aleksei18@gmail.com>
2019-04-16 02:54:30 +0300

    alex

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,9 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  let expression = /\b(([0-1][0-9])|(2[0-3])):([0-5][0-9])\b/;
+  let validTime = str.match(expression);
+  return validTime ? validTime[0] : '';
 }
 
 module.exports = getTime;
