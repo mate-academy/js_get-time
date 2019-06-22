3786fd970d041dd96349e4875b7e30f7cf95188e
OleksandrHlybchenko <logista@meta.ua>
2019-06-22 17:56:00 +0300

    for review

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,10 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const regex = /\b(2[0-3]|[0-1][0-9]):[0-5][0-9]\b/;
+  const result = str.match(regex);
+
+  return result ? result[0] : '';
 }
 
 module.exports = getTime;
