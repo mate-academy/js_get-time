148ef287d029aa6e4869c3b322c123b196c9a64a
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-28 12:26:19 +0300

    fix

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,7 @@
  * @returns {string} - valid time
  */
 const getTime = string => {
-  const pattern = /\b(([0-1][0-9])|(2[0-3])):([0-5][0-9]\b)/;
+  const pattern = /\b(([0-1][0-9])|(2[0-3])):([0-5][0-9])\b/;
   const matches = string.match(pattern);
 
   return matches ? matches[0] : '';
