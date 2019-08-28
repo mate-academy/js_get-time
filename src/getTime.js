58c0000952ab91bf95200eb67dc7fa8bef566969
ivan_safiulin <ivansafiulin@myrambler.ru>
2019-08-28 22:40:57 +0300

    Solution

--- src/getTime.js
+++ src/getTime.js
@@ -6,7 +6,9 @@
  * Function takes string and returns first valid time in this string. Valid time
  * has format 'hh:mm'. Both hours and minutes have 2 digits, e.g. '09:00'. Hours
  * is at most 23 and minutes is at most 59. '37:98' is not valid time. If
- * correct time not found the function returns empty string.
+ * correct time not found the function returns empty string
+ * 'Breakfast at 09:60, Dinner at 21:00'.
+ * 'Breakfast at 09:59, Dinner at 21:00'
  *
  * getTime('Breakfast at 09:00') === '09:00'
  * getTime('Breakfast at 09:60, Dinner at 21:00') === '21:00'
@@ -17,19 +19,12 @@
  *
  * @returns {string} - valid time
  */
+
 function getTime(str) {
-  const pattern = /\d+:\d+/g;
-  const times = str.match(pattern);
+  const pattern = /\b([0-2][0-3]|[0-1][0-9]):[0-5][0-9]\b/g;
+  const matchesTimes = str.match(pattern);
 
-  const findTime = times.find((time) => {
-    const partTime = time.split(':');
-    return (Number(partTime[0]) < 24
-      && partTime[0].length <= 2
-      && partTime[1].length <= 2
-      && Number(partTime[1] < 60));
-  });
-
-  return findTime || '';
+  return (matchesTimes) ? matchesTimes[0] : '';
 }
 
 module.exports = getTime;
