545d17bb53296f590709acb0bd46a53cadd9dd20
amserhii11 <amelin.serhii@gmail.com>
2019-08-28 09:03:20 +0300

    Add solution

--- src/getTime.js
+++ src/getTime.js
@@ -18,8 +18,8 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  const strSymb = /(^|\D)((([0-1]\d)|(2[0-3])):[0-5]\d)(\D|$)/;
-  return strSymb.test(str) ? str.match(strSymb)[2] : '';
+  const pattern = /(^|\D)((([0-1]\d)|(2[0-3])):[0-5]\d)(\D|$)/;
+  return pattern.test(str) ? str.match(pattern)[2] : '';
 }
 
 module.exports = getTime;
