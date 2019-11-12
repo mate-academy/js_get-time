4db98609bcef0e21eb4bff81c3fa9b752fc53119
Oles Marola <oles_pw@ukr.net>
2019-11-12 23:56:18 +0200

    getTime

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,8 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const timeFromStr = str.match(/\b([0-1][0-9]|[2][0-3]):[0-5][0-9]\b/);
+  return timeFromStr ? timeFromStr[0] : '';
 }
 
 module.exports = getTime;
