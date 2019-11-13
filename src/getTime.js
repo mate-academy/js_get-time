825b449d7acb06cba2a99cec644b1b4b67f629b4
PavloPantus <mr.pantus@gmail.com>
2019-11-13 16:18:32 +0200

    solution

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,12 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const timeAtString = str
+    .match(/\b[0-1][0-9]:[0-5][0-9]\b|\b[2][0-3]:[0-5][0-9]\b/m);
+  if (timeAtString === null) {
+    return '';
+  }
+  return timeAtString.join('');
 }
 
 module.exports = getTime;
