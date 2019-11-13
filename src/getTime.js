6322630a401186ccdec70a6494823c0e1e000b68
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-13 21:59:03 +0200

    Aidinov Alexii

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,10 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const pattern = /\b(([0-1][0-9])|([0-2][0-3])):[0-5][0-9]\b/;
+  const matchedTime = str.match(pattern);
+
+  return (matchedTime === null) ? '' : matchedTime[0];
 }
 
 module.exports = getTime;
