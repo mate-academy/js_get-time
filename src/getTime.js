dd81dce143a1a3b0b02679bdb2df0580428154df
vadim-os <osinsky_vadim@yahoo.com>
2019-11-13 17:43:26 +0200

    solution

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,7 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  const timePosition = str.search(/\b([01][0-9]|2[0-3]):[0-5][0-9]\b/);
+  const timePosition = str.search(/\b([01]\d|2[0-3]):[0-5]\d\b/);
 
   return (timePosition !== -1) ? str.substr(timePosition, 5) : '';
 }
