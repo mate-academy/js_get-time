c1adbeef78a4d912a201703fe760e53845858cb8
Viktoria <valencia88vika@gmail.com>
2020-01-22 16:03:19 +0200

    getTime

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,13 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const regStr = /\b([0-1][0-9]|[2][0-3]):[0-5][0-9]\b/;
+
+  if (regStr.test(str)) {
+    return str.match(regStr)[0];
+  } else {
+    return '';
+  }
 }
 
 module.exports = getTime;
