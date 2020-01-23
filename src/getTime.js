5b7c969b8756fbec5e5b1fb6d76b7c7407f0281a
Stanislav Mochyr <delenjer@gmail.com>
2020-01-23 12:52:06 +0200

    js_get-time

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,9 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const correctTime = str.match(/\b([0-1]\d|2[0-3]):[0-5]\d\b/g);
+
+  return (correctTime) ? correctTime[0] : '';
 }
 
 module.exports = getTime;
