0472184dba3cba791833d07e38043b9be21863ee
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-23 16:28:54 +0200

    fix ternary

--- src/getTime.js
+++ src/getTime.js
@@ -21,7 +21,7 @@ function getTime(str) {
   const timeRegEx = /\b(?:[0-1][0-9]|2[0-3]):[0-5][0-9]\b/;
   const propTime = str.match(timeRegEx);
 
-  return propTime === null ? '' : propTime.join('');
+  return propTime ? propTime.join('') : '';
 }
 
 module.exports = getTime;
