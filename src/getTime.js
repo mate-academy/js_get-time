0456c9db377ec60fdc41b9b9eab260f00f56ef14
Yevhenii <DrozhzhynY>
2020-03-27 17:31:08 +0200

    solution

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,37 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const strTime = str.match(/[0-9]{2,3}:[0-9]{2,3}/g);
+
+  for (const i of strTime) {
+    const hours = i.slice(0, 2);
+    const minutes = i.slice(3, 5);
+
+    if (i.length > 5) {
+      continue;
+    } else if (hours < 24 && minutes < 60) {
+      return i;
+    }
+  }
+
+  return '';
+  // const result = str.match(/\b[0-9][0-9]:[0-9][0-9]\b/).join();
+
+  // return result;
+  // const strNum = str.match(/[0-9]/gi).join('');
+  // const firstTwo = strNum.slice(0, 2);
+  // const lastTwo = strNum.slice(2, 4);
+  // const result = firstTwo + ':' + lastTwo;
+
+  // if ((firstTwo >= '24') || lastTwo > '59') {
+  //   return '';
+  // }
+
+  // if (strNum.slice(0, 3) === '000:12') {
+  //   return '01:12';
+  // }
+
+  // return result;
 }
 
 module.exports = getTime;
