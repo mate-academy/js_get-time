4739f182e38b4603207f4d9f90ba6bb97ad50b0e
BorisovecViktor <viktorborisovets@gmail.com>
2020-03-27 16:39:57 +0200

    solution

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,13 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const regExpTime = str.match(/\b(?:[0-1][0-9]|2[0-3]):[0-5][0-9]\b/);
+
+  if (regExpTime) {
+    return regExpTime.join('');
+  } else {
+    return '';
+  }
 }
 
 module.exports = getTime;
