cd17d415d1804078166524c6c0903227a4ea21e8
DTR <t.terry.rice@gmail.com>
2020-05-25 19:14:23 +0300

    final 1.0

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,13 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  // write code here
+  const time = str.match(/(\D|^)((([2][0-3])|([0-1][0-9])):[0-5][0-9])(\D|$)/);
+
+  if (time !== null) {
+    return time[2];
+  };
+
+  return '';
 }
 
 module.exports = getTime;
