039343dcd404eb0d8177a4acf86734735baa44ec
DTR <t.terry.rice@gmail.com>
2020-05-26 14:20:43 +0300

    final 1.3

--- src/getTime.js
+++ src/getTime.js
@@ -18,7 +18,7 @@
  * @returns {string} - valid time
  */
 function getTime(str) {
-  const time = str.match(/(\D|^)((([2][0-3])|([0-1][0-9])):[0-5][0-9])(\D|$)/);
+  const time = str.match(/(\b)((([2][0-3])|([0-1][0-9])):[0-5][0-9])(\b)/);
 
   return (time !== null) ? time[2] : '';
 }
